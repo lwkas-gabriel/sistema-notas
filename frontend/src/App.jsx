ec03d5d8c44672f729b98173b98bafe142fb6802
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-03-18 18:22:48 -0300

    feat: building initial frontend interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='container-lg bg-dark text-light'>
+      <h1 className='text-light'>Sistema de Notas</h1>
+      <form className='container text-light'>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Nome</label>
+            <input className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Texto</label>
+            <input className="form-control" id="exampleInputPassword1" />
+          </div>
+          <button className='btn btn-success mb-4'>Adicionar nota</button>
+      </form>
+
+      <h2>Lista de Notas</h2>
+      <ul className='list-group'>
+        {/* inicialmente vazia */}
+        <li className='list-group-item'>Teste</li>
+        <li className='list-group-item'>Teste</li>
+        <li className='list-group-item'>Teste</li>
+        <li className='list-group-item'>Teste</li>
+        <li className='list-group-item'>Teste</li>
+      </ul>
+    </main>
   )
 }
 
