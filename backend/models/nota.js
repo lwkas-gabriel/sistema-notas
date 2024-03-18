ef46577d96e64b2e76926403bdc6b0456ad61427
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-03-18 17:32:46 -0300

    feat: creating iinitial models and migrations

---
+++ backend/models/nota.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Nota extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Nota.init({
+    title: DataTypes.STRING,
+    context: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Nota',
+  });
+  return Nota;
+};
